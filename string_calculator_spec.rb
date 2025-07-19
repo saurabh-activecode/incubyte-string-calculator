9848d92be499c35465751fd51ffaea3f3cf0f061
saurabh-activecode <saurabh@activecode.in>
2025-07-19 11:52:18 +0530

    updated specs for basic usecases

--- string_calculator_spec.rb
+++ string_calculator_spec.rb
@@ -5,9 +5,20 @@ RSpec.describe StringCalculator do
   describe '.add' do
 
     context "when the input is empty" do
+      it "returns 0" do
+        expect(described_class.add("")).to eq(0)
+        expect(described_class.add("   ")).to eq(0)
+      end
     end
 
     context 'with default delimiters' do
+      it 'returns the sum of comma-separated numbers' do
+        expect(described_class.add("1,2,3")).to eq(6)
+      end
+
+      it 'returns the sum of numbers separated by newlines' do
+        expect(described_class.add("1\n2\n3")).to eq(6)
+      end
     end
 
     context 'with custom delimiter' do
