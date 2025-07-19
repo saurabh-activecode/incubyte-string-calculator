e09ae99dda9d0dbad5f174b98b580fe41e7f2b4b
saurabh-activecode <saurabh@activecode.in>
2025-07-19 11:53:16 +0530

    method updates for basic use cases

--- string_calculator.rb
+++ string_calculator.rb
@@ -1,4 +1,13 @@
 class StringCalculator
+  DEFAULT_DELIMITERS = /,|\n/
+
   def self.add(numbers)
+    return 0 if numbers.strip.empty?
+
+    delimiters = DEFAULT_DELIMITERS
+
+    num_list = numbers.split(delimiters).map(&:to_i)
+
+    num_list.sum
   end
 end
