72d39ed80e04ae5293e3f8f7f3cf87e41c139451
saurabh-activecode <saurabh@activecode.in>
2025-07-19 12:55:34 +0530

    update use cases and added corner cases

--- string_calculator_spec.rb
+++ string_calculator_spec.rb
@@ -35,6 +35,11 @@ RSpec.describe StringCalculator do
       it 'handles mixed comma and newline delimiters' do
         expect(described_class.add("1\n2,3")).to eq(6)
       end
+
+      it 'handles multiple trailing commas and newline delimiters' do
+        expect(described_class.add("1,2,3,,,")).to eq(6)
+        expect(described_class.add("1\n2\n3\n\n\n")).to eq(6)
+      end
     end
 
     context 'with custom delimiter' do
@@ -42,7 +47,25 @@ RSpec.describe StringCalculator do
         expect(described_class.add("//;\n1;2;3")).to eq(6)
       end
 
+      it 'works with any multiple-character custom delimiter' do
+        expect(described_class.add("//#!\n4#!5#!6")).to eq(15)
+      end
 
+      it 'works in conjunction with existing delimiters' do
+        expect(described_class.add("//;\n1;2,3\n4")).to eq(10)
+      end
+
+      it 'returns 0 when there are no numbers and no delimiters' do
+         expect(described_class.add("//;\n")).to eq(0)
+      end
+
+      it 'returns 0 when there are no numbers but has multiple delimiters' do
+         expect(described_class.add("//;\n;;;")).to eq(0)
+      end
+
+      it 'fallsback to predefined delimiters if custom delimiter is not specified' do
+        expect(described_class.add("//\n1,2,3")).to eq(6)
+      end
     end
 
     context 'with negative numbers' do
@@ -70,6 +93,17 @@ RSpec.describe StringCalculator do
     end
 
     context 'edge cases' do
+      it 'returns 0 when there are no numbers after the custom delimiter' do
+        expect(described_class.add("//;\n")).to eq(0)
+      end
+
+      it 'ignores whitespace-only input' do
+        expect(described_class.add(" \n ")).to eq(0)
+      end
+
+      it 'handles single number input' do
+        expect(described_class.add("42")).to eq(42)
+      end
     end
   end
 end
