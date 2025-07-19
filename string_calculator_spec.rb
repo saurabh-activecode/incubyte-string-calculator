0c95b33fa79ecefb5104548ad8c9a47ed24d7d4b
saurabh-activecode <saurabh@activecode.in>
2025-07-19 11:43:05 +0530

    adding contexts

--- string_calculator_spec.rb
+++ string_calculator_spec.rb
@@ -3,5 +3,20 @@ require 'rspec'
 
 RSpec.describe StringCalculator do
   describe '.add' do
+
+    context "when the input is empty" do
+    end
+
+    context 'with default delimiters' do
+    end
+
+    context 'with custom delimiter' do
+    end
+
+    context 'with negative numbers' do
+    end
+
+    context 'edge cases' do
+    end
   end
 end
