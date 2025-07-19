40be58fb44750b7643f0835dfd66be15e0fde998
saurabh-activecode <saurabh@activecode.in>
2025-07-19 12:13:51 +0530

    added error handling for negative numbers

--- string_calculator.rb
+++ string_calculator.rb
@@ -8,6 +8,11 @@ class StringCalculator
 
     num_list = numbers.split(delimiters).map(&:to_i)
 
+    negatives = num_list.select { |n| n < 0 }
+    unless negatives.empty?
+      raise "negative numbers not allowed: #{negatives.join(',')}"
+    end
+
     num_list.sum
   end
 end
